3e7c6268736e833205e3aeaf363088a4d30b1fe0
Todd Sundsted <todd@sundsted.com>
2020-02-16 11:16:07 -0500

    Implement protocol.

--- spec/iterm2_spec.cr
+++ spec/iterm2_spec.cr
@@ -7,4 +7,60 @@ Spectator.describe Iterm2 do
       expect(Iterm2::VERSION).to eq(version)
     end
   end
+
+  let(:output) { IO::Memory.new(10) }
+  subject { described_class.new(output: output) }
+
+  context "in ITerm2" do
+    CMD_RE = /^\e]1337;File=([^=]+=[^;:]+(;[^=]+=[^;:]+)*)?:([a-zA-Z0-9+\=]*)\a$/
+
+    describe "#display" do
+      it "wraps the image in the protocol envelop" do
+        subject.display(IO::Memory.new("123"))
+        expect(output.rewind.read_line).to match(CMD_RE)
+      end
+
+      it "base64-encodes the image data" do
+        subject.display(IO::Memory.new("123"))
+        match = output.rewind.read_line.match(CMD_RE).not_nil!
+        expect(match[match.size - 1]).to eq("MTIz")
+      end
+
+      it "specifies the name" do
+        subject.display(IO::Memory.new("123"), name: "test")
+        match = output.rewind.read_line.match(CMD_RE).not_nil!
+        expect(match[1].split(";")).to have("name=dGVzdA==")
+      end
+
+      it "specifies the size" do
+        subject.display(IO::Memory.new("123"), size: 3)
+        match = output.rewind.read_line.match(CMD_RE).not_nil!
+        expect(match[1].split(";")).to have("size=3")
+      end
+
+      it "specifies the width to render" do
+        subject.display(IO::Memory.new("123"), width: 100)
+        match = output.rewind.read_line.match(CMD_RE).not_nil!
+        expect(match[1].split(";")).to have("width=100")
+      end
+
+      it "specifies the height to render" do
+        subject.display(IO::Memory.new("123"), height: "100%")
+        match = output.rewind.read_line.match(CMD_RE).not_nil!
+        expect(match[1].split(";")).to have("height=100%")
+      end
+
+      it "toggles preserve aspect ratio off" do
+        subject.display(IO::Memory.new("123"), preserve_aspect_ratio: false)
+        match = output.rewind.read_line.match(CMD_RE).not_nil!
+        expect(match[1].split(";")).to have("preserveAspectRatio=0")
+      end
+
+      it "toggles inline display off" do
+        subject.display(IO::Memory.new("123"), name: "-", inline: false)
+        match = output.rewind.read_line.match(CMD_RE).not_nil!
+        expect(match[1].split(";")).not_to have("inline=1")
+      end
+    end
+  end
 end
--- src/iterm2.cr
+++ src/iterm2.cr
@@ -3,4 +3,61 @@
 #
 class Iterm2
   VERSION = {{ `shards version "#{__DIR__}"`.chomp.stringify }}
+
+  # Creates a new instance.
+  #
+  def initialize(@output : IO = STDOUT)
+  end
+
+  # Wraps the input in the protocol envelop and writes the result to
+  # the output.
+  #
+  # By default, ITerm2 will display supported image formats within the
+  # terminal. It will download unsupported image formats and non-image
+  # inputs.
+  #
+  # If downloading, `name` is the filename of the downloaded file (it
+  # defaults to "Unnamed file") and "size" is the file size in bytes
+  # (it is optional and only used by the ITerm2 progress indicator).
+  #
+  # `width` and `height` are the width and height with which to
+  # display the image. `width` and `height` are specified by a number
+  # followed by an optional unit, or the word "auto". Units are pixels
+  # ("px") or the percent ("%") of the session's width or height. A
+  # number without a unit specifies character cells.
+  #
+  # If `preserve_aspect_ratio` is false, then the image's inherent
+  # aspect ratio will not be respected; otherwise, it will fill the
+  # specified width and height as much as possible without stretching.
+  # It defaults to `true`.
+  #
+  # If `inline` is false, the input will be downloaded with no visual
+  # representation in the terminal session. Otherwise, it will be
+  # displayed inline. It defaults to `true`.
+  #
+  def display(input : IO, name = nil, size = nil, width = nil, height = nil,
+              preserve_aspect_ratio : Bool = true, inline : Bool = true)
+    unless input.responds_to?(:to_slice)
+      temp =
+        input.responds_to?(:size) ?
+          IO::Memory.new(input.size) :
+          IO::Memory.new
+        IO.copy(input, temp)
+        input = temp
+    end
+    options = [] of String
+    options << "name=#{Base64.strict_encode(name)}" if name
+    options << "size=#{size}" if size
+    options << "width=#{width}" if width
+    options << "height=#{height}" if height
+    options << "preserveAspectRatio=0" unless preserve_aspect_ratio
+    options << "inline=1" if inline
+    @output << "\e]1337;File="
+    @output << options.join(";") unless options.empty?
+    @output << ":"
+    Base64.strict_encode(input, @output)
+    @output << "\a"
+    @output << "\n"
+    self
+  end
 end
